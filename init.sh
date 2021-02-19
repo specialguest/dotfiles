12023e2c248a70764f09cf312e73c83e9e29796f
Julien <julien.garin@gmail.com>
2021-02-19 22:43:13 +0100

    Add google cloud sdk cli + Terraform

---
+++ init.sh
@@ -0,0 +1,3 @@
+brew bundle
+source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
